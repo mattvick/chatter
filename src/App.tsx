2b6e73f5d1f2b44021eb7fb544a3e558ea493f02
Matthew <vickery.matthew@gmail.com>
2021-03-18 15:39:16 +0100

    Mic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
+
 import './App.scss';
+import { recognition } from './speechRecognition';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="btn btn-primary"
+        onClick={() => {
+          console.log('onClick()');
+        }}
+        onContextMenu={() => {
+          console.log('onContextMenu()');
+        }}
+        onDoubleClick={() => {
+          console.log('onDoubleClick()');
+        }}
+        onDrag={() => {
+          console.log('onDrag()');
+        }}
+        onDragEnd={() => {
+          console.log('onDragEnd()');
+        }}
+        onDragEnter={() => {
+          console.log('onDragEnter()');
+        }}
+        onDragExit={() => {
+          console.log('onDragExit()');
+        }}
+        onDragLeave={() => {
+          console.log('onDragLeave()');
+          recognition.stop();
+        }}
+        onDragOver={() => {
+          console.log('onDragOver()');
+        }}
+        onDragStart={() => {
+          console.log('onDragStart()');
+        }}
+        onDrop={() => {
+          console.log('onDrop()');
+        }}
+        onMouseDown={() => {
+          console.log('onMouseDown()');
+          recognition.start();
+        }}
+        onMouseEnter={() => {
+          console.log('onMouseEnter()');
+        }}
+        onMouseLeave={() => {
+          console.log('onMouseLeave()');
+        }}
+        onMouseMove={() => {
+          console.log('onMouseMove()');
+        }}
+        onMouseOut={() => {
+          console.log('onMouseOut()');
+        }}
+        onMouseOver={() => {
+          console.log('onMouseOver()');
+        }}
+        onMouseUp={() => {
+          console.log('onMouseUp()');
+          recognition.stop();
+        }}
+      >
+        Hold to speak <FontAwesomeIcon icon={faMicrophone} />
+      </button>
     </div>
   );
 }
